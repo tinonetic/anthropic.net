7aac71ab56bf0c0837af44b8efcc347f9fe2d901
tinonetic <8109147+tinonetic@users.noreply.github.com>
2023-04-16 01:21:24 +0200

    Add Example project Suppressed code analysis errors for example

---
+++ Examples/AnthropicNetDemo/Program.cs
@@ -0,0 +1,16 @@
+namespace AnthropicNetDemo;
+
+/// <summary>
+/// Basic Demo console program that shows library usage.
+/// </summary>
+internal sealed class Program
+{
+    /// <summary>
+    /// Main entry method.
+    /// </summary>
+    public static void Main()
+    {
+        // Hello
+        Console.WriteLine("Hello, World!");
+    }
+}
