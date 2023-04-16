aa2624c1eeb9cda47d9f85fa11fcb9aeece6cb54
tinonetic <8109147+tinonetic@users.noreply.github.com>
2023-04-16 15:38:40 +0200

    Add global suppressions for code and style analysis

---
+++ Tests/Anthropic.Net.Tests/GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Preferred convention", Scope = "module")]
+[assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1309:Field names should not begin with underscore", Justification = "Preferred convention", Scope = "module")]
+[assembly: SuppressMessage("Style", "IDE0009:Member access should be qualified.", Justification = "Preferred convention", Scope = "module")]
+[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "Preferred convention", Scope = "module")]
+[assembly: SuppressMessage("Style", "IDE0022:Use expression body for method", Justification = "Preferred convention", Scope = "module")]
+[assembly: SuppressMessage("Style", "IDE0021:Use expression body for constructor", Justification = "Preferred convention", Scope = "module")]
