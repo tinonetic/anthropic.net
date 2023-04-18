feaddff4e59317bc5fa3eef0f3f603ef0b843c9b
tinonetic <8109147+tinonetic@users.noreply.github.com>
2023-04-18 18:16:50 +0200

    Add new strongly typed CompleteResponse

---
+++ Source/Anthropic.Net/CompleteResponse.cs
@@ -0,0 +1,26 @@
+namespace Anthropic.Net;
+using System.Text.Json.Serialization;
+
+/// <summary>
+/// Represents a response from the Anthropic API for a text completion request.
+/// </summary>
+public class CompleteResponse
+{
+    /// <summary>
+    /// Gets or sets the resulting completion up to and excluding the stop sequences.
+    /// </summary>
+    [JsonPropertyName("completion")]
+    public string? Completion { get; set; }
+
+    /// <summary>
+    /// Gets or sets the reason why the text completion process stopped.
+    /// </summary>
+    [JsonPropertyName("stop_reason")]
+    public string? StopReason { get; set; }
+
+    /// <summary>
+    /// Gets or sets the actual stop sequence that was seen, if the stop reason was "stop_sequence".
+    /// </summary>
+    [JsonPropertyName("stop")]
+    public string? Stop { get; set; }
+}
