1198d19d80a3e1525a61802a47abefda39f8035d
tinonetic <8109147+tinonetic@users.noreply.github.com>
2023-04-19 22:22:48 +0200

    Rename CompletionRequest Refactor and change class structure

--- Source/Anthropic.Net/CompletionRequest.cs
+++ Source/Anthropic.Net/CompletionRequest.cs
@@ -1,34 +1,42 @@
 namespace Anthropic.Net;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
+using Anthropic.Net.Constants;
 
 /// <summary>
 /// Represents a request to complete text using the Anthropic API.
 /// </summary>
-public class CompleteRequest
+public class CompletionRequest
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="CompleteRequest"/> class with the specified prompt and model identifier.
+    /// Initializes a new instance of the <see cref="CompletionRequest"/> class with the specified prompt and model identifier.
     /// </summary>
-    /// <param name="prompt">The prompt you want Claude to complete.</param>
-    /// <param name="model">The version of Claude to use for the request.</param>
-    public CompleteRequest(string prompt, string model)
+    /// <param name="userQuestion">The Question or Instruction that forms part of the prompt you want Claude to complete.</param>
+    /// <param name="model">The version of Claude to use for the request. See consants from <see cref="AnthropicModels"/> and API documentation.</param>
+    /// <param name="maxTokensToSample">A maximum number of tokens to generate before stopping.</param>
+    public CompletionRequest(string userQuestion, string model, int maxTokensToSample = 300)
     {
-        Prompt = prompt;
         Model = model;
-        MaxTokensToSample = 10;
-        StopSequences = new[] { "\n\nHuman:" };
+        MaxTokensToSample = maxTokensToSample;
+        StopSequences = new List<string>() { "\\n\\nHuman:" };
         Stream = false;
         Temperature = 1;
         TopK = -1;
         TopP = -1;
+        UserQuestion = string.IsNullOrWhiteSpace(userQuestion) ? "Hi" : userQuestion;
     }
 
     /// <summary>
-    /// Gets or sets the prompt you want Claude to complete.
+    /// Gets or sets the question or instruction part of the prompt.
+    /// </summary>
+    [JsonIgnore]
+    public string UserQuestion { get; set; }
+
+    /// <summary>
+    /// Gets the prompt you want Claude to complete.
     /// </summary>
     [JsonPropertyName("prompt")]
-    public string Prompt { get; set; }
+    public string Prompt => $"\\n\\n{AnthropicSignals.HumanSignal}{UserQuestion}{AnthropicSignals.HumanSignal}";
 
     /// <summary>
     /// Gets or sets the version of Claude to use for the request.
@@ -46,7 +54,7 @@ public class CompleteRequest
     /// Gets or sets a list of strings upon which to stop generating.
     /// </summary>
     [JsonPropertyName("stop_sequences")]
-    public IEnumerable<string> StopSequences { get; set; }
+    public List<string> StopSequences { get; set; }
 
     /// <summary>
     /// Gets or sets a value indicating whether to incrementally stream the response using SSE.
